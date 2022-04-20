565b67f13bfb67d1a06b9133e88d7ff0cff52e09
Grace Kosieradzki <kosier2@stolaf.edu>
2022-04-19 22:37:15 -0500

    state methods

---
+++ state.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include"State.h"
+#include"/usr/local/cs/cs251/show_mem.h"
+
+void State::read_from(const char *mem){
+    name = _get_tilde_terminated_string(mem);
+    mem += (name.size() + 1);
+    age = _get_int(mem, 2);
+    mem += 2;
+    bio = _get_tilde_terminated_string(mem);
+    mem += (bio.size() + 1);
+    percentMatch = _get_int(mem, 2);
+    mem += 2;
+}
+
+void State::write_to(char *mem){
+    _put_tilde_terminated_string(name, mem);
+    mem += (name.size() + 1);
+    _put_int(age, mem, 2);
+    mem += 2;
+    _put_tilde_terminated_string(bio, mem);
+    mem += (bio.size() + 1);
+    _put_int(percentMatch, mem, 2);
+    mem += 2;
+}
