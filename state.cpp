1843c28f91de332b89fc23098d5600b8ea344c08
Grace Kosieradzki <kosier2@stolaf.edu>
2022-04-20 15:32:08 -0500

    yaml and text files

--- state.cpp
+++ state.cpp
@@ -6,6 +6,8 @@ using namespace std;
 void State::read_from(const char *mem){
     showPopUp = _get_char(mem, 1);
     mem += 1;
+    popUpText = _get_tilde_terminated_string(mem);
+    mem += (popUpText.size() + 1);
     name = _get_tilde_terminated_string(mem);
     mem += (name.size() + 1);
     age = _get_int(mem, 2);
@@ -19,6 +21,8 @@ void State::read_from(const char *mem){
 void State::write_to(char *mem){
     _put_char(showPopUp, mem);
     mem += 1;
+    _put_tilde_terminated_string(popUpText, mem);
+    mem += (popUpText.size() + 1);
     _put_tilde_terminated_string(name, mem);
     mem += (name.size() + 1);
     _put_int(age, mem, 2);
