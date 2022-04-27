1ab8c6bbc7cb6c8c4488ec9f189a110dd3a98196
Jc <loza1@stolaf.edu>
2022-04-26 20:33:35 -0500

    Edits on state and user class

--- state.h
+++ state.h
@@ -5,20 +5,18 @@
 using namespace std;
 
 class State{
-    Account &match;
-    int  numMatches;
+
+    Account &potmatch;
+    int percentMatch, numMatches;
     string popUpText, pageTitle;
     char showPopUp;
 
     public:
     char getShowPopUp() const {return showPopUp;}
     int getPercentMatch() const {return percentMatch;}
-    int getAge() const{return age;}
     int getnumMatches() const {return numMatches;}
     string getPageTitle() const {return pageTitle;}
     string getPopUpText()const{return popUpText;}
-    string getName() const{return name;}
-    string getBio() const{return bio;}
     void read_from(const char *mem);
     void write_to(char *mem);
 };
