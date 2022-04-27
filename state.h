bdec78d178f9d2e389a6e0f88a8b724b4fc622f8
Grace Kosieradzki <kosier2@stolaf.edu>
2022-04-27 16:12:17 -0500

    offset function

--- state.h
+++ state.h
@@ -5,21 +5,20 @@
 using namespace std;
 
 class State{
-    int percentMatch, age, numMatches, numFields;
-    string name, bio;
-    char showPopUp, pageTitle, popUpText;
+    int percentMatch, age, numMatches, numFields, bioLength, nameLength, popUpTextLength;
+    string name, bio, popUpText;
+    char showPopUp, pageTitle;
 
     //showPopUp: 0- no pop up, 1- y/n pop up, 2- enter text pop up
     //pageTitle: M- main page, L- chat list, C- user chat
-    //popUpText: S- same match, M- more than friends, J- just friends, B- block user, R- report user, E- explain
 
     public:
     char getShowPopUp() const {return showPopUp;}
+    char getPageTitle() const {return pageTitle;}
     int getPercentMatch() const {return percentMatch;}
     int getAge() const{return age;}
     int getnumMatches() const {return numMatches;}
-    int offsetOf(int index) const;
-    string getPageTitle() const {return pageTitle;}
+    int offset(string text);
     string getPopUpText()const{return popUpText;}
     string getName() const{return name;}
     string getBio() const{return bio;}
