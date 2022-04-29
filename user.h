b85bdc39f7c29c5f98d549b703bb41e5ad4a7fe8
Jc <loza1@stolaf.edu>
2022-04-29 09:29:35 -0500

    updates on user.h

--- user.h
+++ user.h
@@ -17,24 +17,18 @@ class Account{
     string pronouns;
     Account **matches;
     int numMatches;
-    int month;
-    int day;
-    int year;
     int age;
 
     Account(string eml, string pwd){
       set_email(eml);
       set_password(pwd);
-<<<<<<< HEAD
       set_bio("n/a");
       set_name("n/a");
       matches = 0;
       numMatches = 0;
       set_month(0);
       set_prounouns("n/a");
-      set_year("n/a");
-      set_day("n/a");
-      set_age("n/a");
+
     }
       Account(){
       set_email("n/a");
@@ -45,21 +39,6 @@ class Account{
       numMatches = 0;
       set_month(0);
       set_prounouns("n/a");
-      set_year("n/a");
-      set_day("n/a");
-      set_age("n/a");
-=======
-      set_bio("none");
-      set_firstName("none");
-      set_lastName("none");
-      matches = 0;
-      numMatches = 0;
-      set_month(0);
-      set_prounouns("none");
-      set_year(0);
-      set_day(0);
-      set_age(0);
->>>>>>> 0636f536602733b0b93c476a37c7636d43afd66a
     }
 
   public:
